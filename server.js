b1b8c8980e866ebb7108a58d50e19bbed1f9f94f
Utopianboy99 <madnhlaka@gmail.com>
2025-05-02 06:23:01 +0200

    comment fall back route

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
